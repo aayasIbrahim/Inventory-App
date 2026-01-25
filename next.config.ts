aa689a0c7d35c422949f36210bb23838c086b046
Ayas Ibrahim <aayasabraham@gmail.com>
2026-01-25 14:59:33 +0600

    next config edited

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    turbopack: false, // Turbopack OFF
-  },
+  
   typescript: {
-    ignoreBuildErrors: true, // Dev faster, optional
+    ignoreBuildErrors: true,
   },
 };
 
